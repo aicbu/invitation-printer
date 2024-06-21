6aa8f907e16e52d7963b72cac06488673f29c9b6
Nethmi Ranasinghe <73777468+NethmiRanasinghe@users.noreply.github.com>
2024-06-21 07:54:30 +0000

    correct image twice saving issue

--- app.py
+++ app.py
@@ -1,47 +1,27 @@
 import streamlit as st
 from PIL import Image, ImageDraw, ImageFont
-import os
+import io
 
 def add_text_to_image(text):
     # Open the JPEG image
     image = Image.open("invitation_card.jpeg")
-
     width, height = image.size
-
     # Create a drawing object
     draw = ImageDraw.Draw(image)
-
     # Define the font for the text
     font = ImageFont.truetype("helvetica.ttf", 35)
-
     # Specify the text and position
-    # text_width, text_height = draw.textsize(text, font)  # Get the width and height of the text
-
     left, top, right, bottom = font.getbbox(text)
     text_width = right - left
-
     # Calculate the x-coordinate to center the text
     x = (width - text_width) / 2
     y = 730  # Adjust the y-coordinate as needed
-
     # Add the text to the image
     draw.text((x, y), text, font=font, fill=(169, 104, 0))
-
     return image
 
 st.title("Add Invitees")
-
 text = st.text_input("Enter the name of the invitee (Ex - Mr & Mrs Ranasinghe & family):")
-
-# downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
-# modified_images_folder = os.path.join(downloads_path, "Modified Invitations")
-# if not os.path.exists(modified_images_folder):
-#     os.makedirs(modified_images_folder)
-
-# output_path = os.path.join(modified_images_folder, f"{text}.jpeg")
-
-output_path =  os.path.join('images/', f"{text}.jpeg")
-print(output_path)
 
 if st.button("Add the name"):
     if text:
@@ -50,16 +30,17 @@ if st.button("Add the name"):
         # Display the modified image
         st.image(modified_image, caption="Modified Invitation")
         
-        # Save the image to the specified folder
-        modified_image.save(output_path)
+        # Convert the image to bytes
+        img_byte_arr = io.BytesIO()
+        modified_image.save(img_byte_arr, format='JPEG')
+        img_byte_arr = img_byte_arr.getvalue()
         
         # Create a download button
-        with open(output_path, "rb") as file:
-            st.download_button(
-                label="Download the modified invitation",
-                data=file,
-                file_name=f"{text}.jpeg",
-                mime="image/jpeg"
-            )
+        st.download_button(
+            label="Download the modified invitation",
+            data=img_byte_arr,
+            file_name=f"{text}.jpeg",
+            mime="image/jpeg"
+        )
     else:
         st.write("Please enter text.")
